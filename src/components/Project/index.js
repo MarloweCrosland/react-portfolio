aa37a33ad06f3db373724a9f277cc5b47e5745e0
marlowe <marlowepdx@gmail.com>
2022-09-18 13:30:45 -0700

    added bootstrap to create thumbnails rows and columns

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,37 +4,25 @@ import React from 'react';
 export default function Project() {
   return (
     <div class="container">
-        <h1 class="fw-light text-center text-lg-start mt-4 mb-0">My Projects</h1>
+        <h1 className="fw-light text-center text-lg-start mt-4 mb-0">My Projects</h1>
+    <div className="row">
+        <div className='gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe'>
+            <img src={require('../../assets/project-1/img-1.png')} className='img-thumbnail' alt='Sample' />
+        </div>
 
-        <div className='bg-image'>
-            <img src={require('../../assets/project-1/img-1.png')} className='img-fluid' alt='Sample' />
-            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 1)' }}>
-            <div className='d-flex justify-content-center align-items-center h-100'>
-            <p className='text-white mb-0'>Can you see me?</p>
-            </div>
+        <div className='gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe'>
+            <img src={require('../../assets/project-3/mountain+background.png')} className='img-thumbnail' alt='Sample' />
+        </div>
 
-            {/* <img src={require('../../assets/project-1/img-1.png')} className='img-fluid' alt='Sample' />
-            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 1)' }}>
-            <div className='d-flex justify-content-center align-items-center h-100'>
-            <p className='text-white mb-0'>Can you see me?</p>
-            </div>
+        <div className='gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe'>
+            <img src={require('../../assets/project-4/mountain+background.png')} className='img-thumbnail' alt='Sample' />
+        </div>
 
-            <img src={require('../../assets/project-1/img-1.png')} className='img-fluid' alt='Sample' />
-            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 1)' }}>
-            <div className='d-flex justify-content-center align-items-center h-100'>
-            <p className='text-white mb-0'>Can you see me?</p>
-            </div>
+        <div className='gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe'>
+            <img src={require('../../assets/project-2/img-1.png')} className='img-thumbnail' alt='Sample' />
+        </div>
 
-            <img src={require('../../assets/project-1/img-1.png')} className='img-fluid' alt='Sample' />
-            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 1)' }}>
-            <div className='d-flex justify-content-center align-items-center h-100'>
-            <p className='text-white mb-0'>Can you see me?</p>
-            </div> */}
-      </div>
-    </div>
-  
-  
-
+        </div>
     </div>
   
   );
