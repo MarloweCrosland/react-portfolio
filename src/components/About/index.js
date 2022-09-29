e640d8bf02c8f038601182da87b077b39cfb89f9
marlowe <marlowepdx@gmail.com>
2022-09-29 13:28:12 -0700

    update about me with photo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,13 +5,18 @@ function About() {
 
   return (
     <section className="my-5" >
-      <h1 id="about">Who am I?</h1>
-      <img src={myphoto} id="myphoto" className="rounded mx-auto d-block" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-      </p>
-      </div>
+        <div id="text-container">
+            <h1 id="about">Who am I?</h1>
+            <p id="about-me">
+            Thank you for viewing my portfolio! I am a passionate and driven tech new-comer and I am highly motivated to never stop learning. 
+            I have a knack for communicating effectively and thoughtfully with others. My career goals include helping a team to build something 
+            awesome while strengthening my skills.
+            </p>
+        </div>
+        <div id="photo-container">
+        <img src={myphoto} id="myphoto" className="rounded mx-auto d-block" style={{ width: "100%" }} alt="cover" />
+        </div>
+
     </section>
   )
 }
