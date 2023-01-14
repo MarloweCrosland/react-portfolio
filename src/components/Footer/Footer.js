65d3c4217d71dbdb76f3d851b6fa33f45708e89e
marlowe <marlowepdx@gmail.com>
2023-01-14 15:36:12 -0800

    font color modifications for dark mode/ light mode

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
       <div className="row">
     
         <div className="footer-icons">
-          <a href="https://github.com/MarloweCrosland" target="_blank"><img className="icon" src={require("../../assets/icons/githubkitty.png")} alt="github logo" /></a>
+          <a href="https://github.com/MarloweCrosland" target="_blank"><img className="icon github" src={'https://w7.pngwing.com/pngs/857/611/png-transparent-github-git-hub-logo-icon-thumbnail.png'} alt="github logo" /></a>
           <a href="https://www.linkedin.com/in/marlowe-crosland-338b5a235/" target="_blank"><img className="icon" src={require("../../assets/icons/linkedin-logo-3.png")} alt="linkedin log" /></a>
 
         
