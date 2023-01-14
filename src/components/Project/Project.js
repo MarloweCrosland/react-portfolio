30dab110a8ea102833c463c3932df6f4965ac687
marlowe <marlowepdx@gmail.com>
2023-01-14 12:38:52 -0800

    updated projects layout to columnar design

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { Image } from "react-bootstrap";
 import galleryImg1 from "./galleryImg1.png";
 import galleryImg2 from "./galleryImg2.jpg";
 import "./Project.css";
 
 export default function Project() {
   return (
-    <div class="container project-container">
-      <h1 class="heading font-1">My Projects</h1>
-      <div className="row">
-        <div className="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+    <div className="project_container">
+      <h1 className="project_heading">My Projects</h1>
+      <div className="project">
           <a
-            className="links"
+            className="project_links"
             href="https://mwjones2186.github.io/camp-firecast/"
           >
             <h6 id="heading">Camp Firecast</h6>
@@ -23,8 +21,8 @@ export default function Project() {
           </a>
         </div>
 
-        <div className="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
-          <a className="links" href="https://codetrip-33a81.web.app">
+        <div className="project">
+          <a className="project_links" href="https://codetrip-33a81.web.app">
             <h6 id="heading">Genu: Restaurant Website Generator</h6>
             <img
               src={galleryImg2}
@@ -34,9 +32,9 @@ export default function Project() {
           </a>
         </div>
 
-        <div className="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+        <div className="project">
           <a
-            className="links"
+            className="project_links"
             href="https://mwjones2186.github.io/camp-firecast/"
           >
             <h6 id="heading">Camp Firecast</h6>
@@ -47,7 +45,6 @@ export default function Project() {
             />
           </a>
         </div>
-      </div>
     </div>
   );
 }
